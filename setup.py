0274ddb350b7602d79141f12ae83110b0f4745a9
Sujeong Lee <lemoncookie3@gmail.com>
2023-09-02 12:48:40 -0700

    make it as an installable package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='sjutils',
+    version='0.1.0',    
+    description='Some utility functions',
+    url='https://github.com/sujeong-lee/sjutils',
+    author='Sujeong Lee',
+    author_email='lemoncookie3@gmail.com',
+    license='',
+    packages=['sjutils'],
+    install_requires=[''                  
+                      ],
+
+    classifiers=[      
+        'Programming Language :: Python :: 3',
+    ],
+)
